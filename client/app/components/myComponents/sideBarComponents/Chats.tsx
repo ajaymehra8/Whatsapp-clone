7aaf6b9870596a035edaddb30829cd07085087e8
ajaymehra8 <116863739+ajaymehra8@users.noreply.github.com>
2025-02-28 20:03:18 +0530

    solved some type errors

--- client/app/components/myComponents/sideBarComponents/Chats.tsx
+++ client/app/components/myComponents/sideBarComponents/Chats.tsx
@@ -3,7 +3,7 @@ import { Box, Button } from "@chakra-ui/react";
 import { MdOutlineAddComment } from "react-icons/md";
 import { SlOptionsVertical } from "react-icons/sl";
 import SearchBar from "./SearchBar";
-import { useEffect, useState,useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import ChatCard from "./chatComponents/ChatCard";
 import { useGlobalState } from "@/app/context/GlobalProvider";
 import { getAllChats } from "@/app/utils/api";
@@ -17,7 +17,7 @@ import { ChatType } from "@/app/types/allTypes";
 
 const Chats = () => {
   const [active, setActive] = useState("All");
-  const { user, dark,fetchAgain,chats,setChats } = useGlobalState();
+  const { user, dark, fetchAgain, chats, setChats } = useGlobalState();
   const buttons = ["All", "Unread", "Groups"];
   const divRef = useRef<HTMLDivElement | null>(null);
   const [height, setHeight] = useState<number>(0);
@@ -35,20 +35,25 @@ const Chats = () => {
 
     return () => observer.disconnect(); // Cleanup on unmount
   }, []);
- 
-
 
   const fetchChats = useCallback(async () => {
-    
-    if (user && chats.length<1)
+    if (user && chats.length < 1)
       try {
         const { data } = await getAllChats();
-        
+
         const chats = createChat(data.chats, user?._id);
-        chats.sort((a:ChatType,b:ChatType)=>(
-          (new Date(b.topMessage?.createdAt ?? 0).getTime()) -
-          (new Date(a.topMessage?.createdAt ?? 0).getTime())
-        ));
+        chats.sort((a: ChatType, b: ChatType) => {
+          // Move pinned chats to the top
+          if (a.isPinned !== b.isPinned) {
+            return a.isPinned ? -1 : 1; // Pinned first
+          }
+
+          // Sort by latest topMessage.createdAt
+          return (
+            new Date(b.topMessage?.createdAt ?? 0).getTime() -
+            new Date(a.topMessage?.createdAt ?? 0).getTime()
+          );
+        });
         setChats(chats);
       } catch (err) {
         if (err instanceof AxiosError) {
@@ -59,21 +64,21 @@ const Chats = () => {
           });
         }
       }
-  }, [user,fetchAgain]);
+  }, [user, fetchAgain]);
   useEffect(() => {
     fetchChats();
   }, [fetchChats]);
   return (
     <Box
       borderRight={dark ? "1px solid #222c33" : "1px solid #d1d7db"}
-      width={{base:"100%",md:"95%"}}
-      marginLeft={{base:"0",md:"14%"}}
+      width={{ base: "100%", md: "95%" }}
+      marginLeft={{ base: "0", md: "14%" }}
       overflowY={"hidden"}
     >
       <Box
         position={"fixed"}
         top={"0"}
-        width={{base:"100%",md:"30%"}}
+        width={{ base: "100%", md: "30%" }}
         minH={"17vh"}
         paddingTop={"15px"}
         zIndex={2}
@@ -87,7 +92,15 @@ const Chats = () => {
           width={"100%"}
           padding={"0 15px"}
         >
-          <h3 style={{ fontSize: "22px", fontWeight: 700,fontFamily:"'Fira Sans',sans-serif" }}>Chats</h3>
+          <h3
+            style={{
+              fontSize: "22px",
+              fontWeight: 700,
+              fontFamily: "'Fira Sans',sans-serif",
+            }}
+          >
+            Chats
+          </h3>
           <Box
             display={"flex"}
             justifyContent={"space-between"}
@@ -135,7 +148,7 @@ const Chats = () => {
         width={"100%"}
         display={"flex"}
         flexDirection={"column"}
-        marginTop={height+25+"px"}
+        marginTop={height + 25 + "px"}
         height={"75vh"}
         overflowY={"auto"}
         bg={dark ? "#111b21" : ""}
@@ -160,7 +173,7 @@ const Chats = () => {
           <h1 className="no-item-text">{noItemText}</h1>
         )}
       </Box>
-      <BottomOptions/>
+      <BottomOptions />
     </Box>
   );
 };
